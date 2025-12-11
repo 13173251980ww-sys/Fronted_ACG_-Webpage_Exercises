727cd9b7afd4f22adee6f4451138c6941782aede
amadeus <1685736247@qq.com>
2025-12-11 11:03:39 +0800

    refactor: 派大星个人网站名言利用axios实现

--- PaiDaXing/api/api.js
+++ PaiDaXing/api/api.js
@@ -1,6 +1,5 @@
-async function loadHitoko(){
+function loadHitoko(){
     const container=document.getElementsByClassName("api")[0];
-    fadeout(container.children);
     if(container.children){
         for(var i=0;i<container.children.length;i++){
             container.children[i].remove();
@@ -8,23 +7,25 @@ async function loadHitoko(){
         }
     }
 
-    const json=await fetch('https://v1.hitokoto.cn/?c=a&c=c&c=d&c=k');
-    const data=await json.json();
-    console.log(data);
+    var data=null;
+    axios({
+        method:"get",
+        url:'https://v1.hitokoto.cn/?c=a&c=c&c=d&c=k',
+    }).then(function (resp) {
+        data=resp.data;
+        const content=document.createElement('span');
+        const source=document.createElement('span');
 
-    const content=document.createElement('span');
-    const source=document.createElement('span');
+        content.style.cssText='font-size: 17px;line-height: 1.8;color: #AAA;'
+        source.style.cssText='font-size: 15px;line-height: 1.8;color: #888;'
+        content.innerText = data.hitokoto;
+        source.innerText = " —— " + data.from+" —— ";
 
-    content.style.cssText='font-size: 17px;line-height: 1.8;color: #AAA;'
-    source.style.cssText='font-size: 15px;line-height: 1.8;color: #888;'
-    content.innerText = data.hitokoto;
-    source.innerText = " —— " + data.from+" —— ";
-
-    container.appendChild(content);
-    var br=document.createElement('br');
-    container.appendChild(br);
-    container.appendChild(source);
-    fadein(container.children);
+        container.appendChild(content);
+        var br=document.createElement('br');
+        container.appendChild(br);
+        container.appendChild(source);
+    })
 }
 
 var timeId=null;
@@ -36,33 +37,4 @@ function autoWrite(){
 
 autoWrite();
 
-function fadeout(elements) {
-    Array.from(elements).forEach((element) => {
-        clearInterval(element.timedId);
-        var opcity=10;
-        element.timedId=setInterval(function(){
-            opcity--;
-            if(opcity<=0){
-                clearInterval(element.timedId)
-                element.style.opacity=0;
-            }
-            element.style.opacity=opcity/10;
-        },10);
-    })
-}
 
-//封装淡入动画
-function fadein(elements) {
-    Array.from(elements).forEach((element) => {
-        clearInterval(element.timedId);
-        var opcity=0;
-        element.timedId=setInterval(function(){
-            opcity++;
-            if(opcity>=10){
-                clearInterval(element.timedId)
-                element.style.opacity=1;
-            }
-            element.style.opacity=opcity/10;
-        },10);
-    })
-}
